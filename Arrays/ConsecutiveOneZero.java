6ac314d8cf97bea48342a63d580191c6b49187bd
Shaili <shailidwivedi05@gmail.com>
2024-09-12 01:13:41 +0530

    move zeros to end of the array

--- Arrays/ConsecutiveOneZero.java
+++ Arrays/ConsecutiveOneZero.java
@@ -1,11 +1,37 @@
 package Arrays;
 
 public class ConsecutiveOneZero {
+    // Maximum consecutive one’s (or zeros) in a binary array
+    public static int consecutiveOne(int arr[]) {
+        int count1 = 0;
+        int maxCount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                count1++;
+            } else {
+                count1 =0;
+            }
+            maxCount = Math.max(maxCount, count1);
+        }
+        return maxCount;
+    }
 
-    public static void consecutiveOneZero(int arr[]){
-
+    public static int consecutiveZero(int arr[]){
+        int count0 = 0;
+        int maxCount = 0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==0){
+                count0++;
+            }else{
+                count0 = 0;
+            }
+            maxCount = Math.max(maxCount, count0);
+        }
+        return maxCount;
     }
     public static void main(String[] args) {
-        
+        int arr[] = {0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1};
+        System.out.println(consecutiveOne(arr));
+        System.out.println(consecutiveZero(arr));
     }
 }
---
+++ Arrays/MoveZeros.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class MoveZeros {
+
+    // Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.   
+     public static void moveZeros(int nums[]) {
+        
+        int idx = 0;
+
+        // Step1: move all non-zero elements to the beginning of the array
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[idx] = nums[i];
+                idx++;
+            }
+        }
+
+        // Step 2: fill the rest of the array with zeros
+        for (int i = idx; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+
+        //Step3 : Print the modified array
+        for (int i : nums) {
+            System.out.print(i + "    ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeros(nums);
+    }
+}
